56d53225d96dedb8d3ef355b067f7d625118f654
Carlos Pérez <44160198+CarlosAugusto92@users.noreply.github.com>
2020-09-08 21:38:30 -0500

    Add files via upload
    This code was wrote in python. It can allow recorded a report to record hour by hour of set of boards that have been tested in some test equipment . I will try to improve with the time.

---
+++ Reporthourbyhour.py
@@ -0,0 +1,72 @@
+#from datetime import date
+import os
+import datetime
+
+
+
+def WelcomeToGeneratorReport():
+    print('Generador de reportes hora x hora')
+    print('Developed by Carlos A. Pérez Méndez')
+
+
+def ClearScreen():
+    Cleaner = os.system("cls")
+    return Cleaner
+    
+def Date_and_Time ():
+    today = datetime.datetime.now()
+    return today
+  
+
+def Report_Generator(modelo, modelo_serial, nombre, nomina, turno, Date_Time):
+  
+    file_log  = open('C:/Users/solidworks/AppData/Local/Programs/Python/Python38-32/Scripts/GeneradorTxt/dist/ReportLog.txt', 'a')
+    file_log.write("Fecha y hora: " + Date_Time + os.linesep)
+    file_log.write("Modelo:" + modelo + os.linesep)
+    file_log.write("Serial:"+ modelo_serial + os.linesep)
+    file_log.write("Nombre:" + nombre + os.linesep)
+    file_log.write("WD ID:" + nomina + os.linesep)
+    file_log.write("Turno:" + turno + os.linesep)
+    file_log.write("\n")
+    file_log.close()
+    ClearScreen()
+
+
+
+
+
+def main():
+    
+     
+    while True:
+        
+        print('Ingrese el modelo')
+        modelo = input()
+        print('Ingrese el serial')
+        modelo_serial = input()
+        print('Nombre')
+        nombre = input()
+        print('WD ID:')
+        nomina = input()
+        print('Turno')
+        turno = input()
+        Date_Time = Date_and_Time()
+        Date_Time = str(Date_Time)
+        Report_Generator(modelo, modelo_serial, nombre, nomina, turno, Date_Time)
+
+    
+
+       
+         
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    WelcomeToGeneratorReport()
+    main()
+        
